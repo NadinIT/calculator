c64d163269f1ae1b9109348ea2d4696b41c10420
nadia <nnaverska@gmail.com>
2019-02-09 16:37:21 +0000

    version 1 s Please enter the commit message for your changes. Lines starting

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,32 +6,16 @@ class Calculator {
     public static void main(String[] args) {
         Scanner myNum = new Scanner(System.in);
 
-        /* 1. Ask user to enter number called num1
-           2. if num1 is greater than 10, print error then go back to step 1
-           3. else if num1 is less than 10, stop
-         */
         System.out.println("Enter your expression:");
-//        String expression;
-//        expression = myNum.next();
-
         String expression = myNum.next();
-        //how to make this array two symbols?
-        // 110000+11
-
 
         char[] arr = expression.toCharArray();
-
         char num1 = arr[0];
         char operator = arr[1];
         char num2 = arr[2];
 
         int n1 = Character.getNumericValue(num1);
         int n2 = Character.getNumericValue(num2);
-
-        //if operator = '-' subtract
-        //if operator = '+' add
-        //if operator = "*" multiply
-        //if operator = "/" divide
 
         if (operator == '-') {
             System.out.println(n1 - n2);
@@ -45,4 +29,5 @@ class Calculator {
     }
 
 }
+
 
