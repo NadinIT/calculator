b70f9c22794a781af0dd97603bbef6ec2212de04
nadia <nnaverska@gmail.com>
2019-02-09 22:13:06 +0000

    working

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,27 +6,51 @@ class Calculator {
     public static void main(String[] args) {
         Scanner myNum = new Scanner(System.in);
 
-        System.out.println("Enter your expression:");
-        String expression = myNum.next();
 
-        char[] arr = expression.toCharArray();
-        char num1 = arr[0];
-        char operator = arr[1];
-        char num2 = arr[2];
 
-        int n1 = Character.getNumericValue(num1);
-        int n2 = Character.getNumericValue(num2);
+            System.out.println("Enter first number");
+            int num1 = 0;
+            int num2 = 0;
+            String operator;
+            boolean a = false;
+            while (a == false) {
+                num1 = myNum.nextInt();
+                if (num1 <= 10) {
+//                System.out.println(num1);
+                    a = true;
+                } else {
+                    System.out.println("Try number between 1 and 10");
+                }
+            }
+            System.out.println("Enter operation( +,-,*,/)");
+            operator = myNum.next();
 
-        if (operator == '-') {
-            System.out.println(n1 - n2);
-        } else if (operator == '+') {
-            System.out.println(n1 + n2);
-        } else if (operator == '*') {
-            System.out.println(n1 * n2);
-        } else if (operator == '/') {
-            System.out.println(n1 / n2);
+
+            System.out.println("Enter second number");
+            boolean b = false;
+            while (b == false) {
+                num2 = myNum.nextInt();
+                if (num2 <= 10) {
+                    if (num2==0){
+                        System.out.println("Error! You cant divide by 0.Try somethimg else");
+                    }else{
+                        b=true;
+                    }
+                } else {
+                    System.out.println("Try number between 1 and 10");
+                }
+            }
+            if (operator.equals("-")){
+                System.out.println(num1 - num2);
+            } else if (operator.equals("+")) {
+                System.out.println(num1 + num2);
+            } else if (operator.equals("*")) {
+                System.out.println(num1 * num2);
+            } else if (operator.equals( "/")) {
+                System.out.println(num1 / num2);
+            }
+
         }
-    }
 
 }
 
